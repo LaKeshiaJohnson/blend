035331c86e0984a28447e34df0589857ecb54421
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2017-12-08 14:42:05 -0600

    auth working. registering users and logging in users

--- app/controllers/user_ctrl.js
+++ app/controllers/user_ctrl.js
@@ -4,9 +4,53 @@ console.log("user controller has loaded");
 
 app.controller("userCtrl", function ($scope, $window, userFactory, $location) {
 
+	$scope.user = {
+		first_name: "",
+		last_name: "",
+		email: "",
+		password: ""
+	};
+
+	$scope.signUp = () => {
+		console.log("you clicked on the signup button");
+		const userObj = {"user": $scope.user};
+		const userObjString = JSON.stringify(userObj);
+		userFactory.signUp(userObjString)
+			.then((data) => {
+				$location.url("/categories");
+				// console.log ("DATA FROM USER SUBMIT", data);
+				
+		});
+	};
+
+	/*userFactory.signUp({
+			first_name: $scope.user.first_name,
+			last_name: $scope.user.last_name,
+			email: $scope.user.email,
+			password: $scope.user.password
+		})
+		.then((data) => {
+			console.log("user controller-new user:", data);
+			$location.path("/categories");
+		});
+	};*/
 
 
-	
+	$scope.login = () => {
+		console.log("you clicked on the login button");
+		userFactory.login($scope.user)
+		.then((data) => {
+				$location.url("/categories");
+				// console.log ("DATA BACK TO CONTROLLER", data);
+			});
+	};
+		/*.then (data => {
+			$scope.creds.auth_token = data;
+			console.log("DATA token", data);
+			$location.path("/categories");
+                //$scope.apply();
+		});
+	};*/
 
 });
 
--- app/factories/recipe_factory.js
+++ app/factories/recipe_factory.js
@@ -1,11 +1,15 @@
 "use strict";
 
-app.factory("recipeFactory", function($q, $http, categoryFactory) {
+app.factory("recipeFactory", function($q, $http, categoryFactory, userFactory) {
 
 	const getRecipeDetails = function(recipeId) {
        
         return $q((resolve, reject) => {
-            $http.get(`http://localhost:3000/recipes/${recipeId}`)
+            $http.get(`http://localhost:3000/recipes/${recipeId}`, {headers:
+                {
+                    'Authorization': `${userFactory.authTokenGetter()}`,
+                },
+            })
                 .then((object) => {
                    console.log("RECIPE DETAILS:", object.data);
                    resolve(object.data);
